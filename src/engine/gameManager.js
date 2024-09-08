275cda1cb70e8d0c331d43ecd9b0b5fed907161e
michellimadesigner <michellimadesigner@outlook.com>
2024-09-08 20:42:53 -0300

    style: adjust the board layout

--- src/engine/gameManager.js
+++ src/engine/gameManager.js
@@ -7,23 +7,27 @@ class Game {
     this.body = window.document.querySelector("body");
   }
 
+  createCard(type) {
+    const card = document.createElement("div");
+    card.setAttribute("class", `card dragable ${type}`);
+    return card;
+  }
+
   buildLevel() {
     const board = document.createElement("div");
     board.classList.add("board");
     this.body.appendChild(board);
 
-    // Configura o número de slots (posições) para o nível atual
     for (let i = 0; i < this.levels.cardSlots; i++) {
-      console.log("aqui", this.levels.cardSlots);
       const cardSlot = document.createElement("div");
       cardSlot.classList.add("card-slot");
 
-      // Verifica se o slot é a saída (porta) para o nível atual
       if (this.levels.levelData[this.levels.currentLevel][i] === "door") {
-        cardSlot.classList.add("door-slot");
-        cardSlot.textContent = "Saída";
+        const doorCard = this.createCard("door");
+        cardSlot.appendChild(doorCard);
+        // cardSlot.classList.add("card-slot");
       } else {
-        cardSlot.textContent = "Drop the card here";
+        cardSlot.classList.add("card-slot--empty");
       }
 
       board.appendChild(cardSlot);
